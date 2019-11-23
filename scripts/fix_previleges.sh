b400cdfa96a4072ca0e1d389bb77b812c58362c9
snyderb7 <vijay.thompson@gmail.com>
2019-11-23 16:11:53 +0530

    updated scripts

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x /home/ec2-user/DockerDemo.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
+chmod +x /home/ec2-user/target/DockerDemo.jar
+chmod +x /home/ec2-user/scripts/start_server.sh
+chmod +x /home/ec2-user/scripts/stop_server.sh
 
